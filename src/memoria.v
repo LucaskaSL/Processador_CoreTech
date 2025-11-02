f5f04c6bcd387797355b9fe8e123df962ad6cd92
Manoel <63215619+Manoelmonteiro@users.noreply.github.com>
2025-11-02 19:55:38 -0300

    feat: adicao de reg e do word_index

--- src/memoria.v
+++ src/memoria.v
@@ -6,3 +6,15 @@ module memory (
     input wire [31:0] WriteData,
     output reg [31:0] ReadData
 );
+
+    reg [31:0] mem_array [0:63]; // reg pra permitir a escrita no array, 64 bytes por palavra
+
+    wire [5:0] word_index = MemAddress[7:2]; // 6 bits para indexar 64 palavras
+
+    
+
+
+
+endmodule
+
+
