1dea9f3b3c2de5d148c1f7dbfb8b7fccfd0461a1
Manoel <63215619+Manoelmonteiro@users.noreply.github.com>
2025-11-02 20:13:35 -0300

    feat: verificadores de tamanho e adicao da logica de escrita

--- src/memoria.v
+++ src/memoria.v
@@ -11,7 +11,24 @@ module memory (
 
     wire [5:0] word_index = MemAddress[7:2]; // 6 bits para indexar 64 palavras
 
-    
+    always @(*) begin
+        // a saída é 0.
+        ReadData = 32'h0;
+        if (MemRead) begin
+            //verifica se o esta dentro dos limites (0,63)
+            //se o endreco for maior que 0xFF (255),ele nao ta no espaco
+            ReadData = mem_array[word_index];// ve a posicao do array
+        end
+    end
+    //logica de escrita (store - mov [rx], ry)
+
+    always @(posedge clk) begin
+        if (MemWrite) begin
+            //escreve os dados no indice da palavra selec
+            mem_array[word_index] = WriteData;
+        end
+    end
+
 
 
 
